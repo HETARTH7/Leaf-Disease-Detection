c1df667c8a069ff2ff2c746b67e2c18880370d9b
unknown <hetarth002@gmail.com>
2023-08-13 15:38:00 +0530

    setup backend

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+http = require("http");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(5000, () => console.log("Server is running on port 5000"));
