eab9198f9c368d81a322662914c35556d5524fc8
unknown <hetarth002@gmail.com>
2023-08-14 18:40:26 +0530

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
-import Home from "./components/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import io from "socket.io-client";
-
-const socket = io.connect("http://localhost:5000");
+import React from "react";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                username={username}
-                setUsername={setUsername}
-                room={room}
-                setRoom={setRoom}
-                socket={socket}
-              />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
