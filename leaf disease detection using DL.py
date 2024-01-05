9ee297f26dda8b9d2bf5d56c8aad8587a8ca0a12
Hetarth <hetarth2028@gmail.com>
2024-01-05 16:59:57 +0530

    import dataset

--- leaf disease detection using DL.py
+++ leaf disease detection using DL.py
@@ -3,23 +3,40 @@ import os
 import PIL
 import PIL.Image
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import cv2
 
 batch_size = 32
 img_height = 180
 img_width = 180
-data_dir = './dataset'
+data_dir = './plant_village_dataset'
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
     subset="training",
-    seed=123)
+    seed=123,
+)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
     subset="validation",
-    seed=123)
+    seed=123,
+)
 
 class_names = train_ds.class_names
 print(class_names)
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+
+for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
